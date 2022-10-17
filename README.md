5b1df6a1d928d6031301aeb4956943d97c929769
ro4 <thefrp@foxmail.com>
2022-10-17 21:39:18 +0800

    read me

---
+++ README.md
@@ -0,0 +1,27 @@
+##SpEL Validation
+
+This is a custom validator based on `Jakarta validation-api`, inspired by `org.hibernate.validator.constraints.ScriptAssert`.  
+The source code is so tiny, you can read it as document :)
+
+### Example
+
+```java
+
+@SpELAssert("#p.maxAge > 100") // repeatable annotation
+@SpELAssert(expression = "#_.maxAge > 100", alias = "_") // alias support
+@SpELAssert(
+        expression = {"#p.minAge < #p.maxAge", "and #p.minAge > 10"}, // multi-line expressions
+        message = "Max must greater than min age, min age must greater than 10",
+        reportOn = "minAge"
+)
+public class Config {
+
+    @SpELAssert("@springBean.method(#p)") // spring bean invoke
+    private String name;
+
+    private Integer minAge;
+
+    private Integer maxAge;
+}
+
+```
--- src/main/java/io/github/ro4/spelvalidation/SpELAssert.java
+++ src/main/java/io/github/ro4/spelvalidation/SpELAssert.java
@@ -4,6 +4,31 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
+/**
+ * This is a custom validator based on `Jakarta validation-api`, inspired by `org.hibernate.validator.constraints.ScriptAssert`.
+ * <pre>
+ * {@code
+ * @SpELAssert("#p.maxAge > 100") // repeatable annotation
+ * @SpELAssert(expression = "#_.maxAge > 100", alias = "_") // alias support
+ * @SpELAssert(
+ *         expression = {"#p.minAge < #p.maxAge", "and #p.minAge > 10"}, // multi-line expressions
+ *         message = "Max must greater than min age, min age must greater than 10",
+ *         reportOn = "minAge"
+ * )
+ * public class Config {
+ *
+ *     @SpELAssert("@springBean.method(#p)") // spring bean invoke
+ *     private String name;
+ *
+ *     private Integer minAge;
+ *
+ *     private Integer maxAge;
+ * }
+ * }
+ * </pre>
+ *
+ * @author ro4
+ */
 @Documented
 @Constraint(validatedBy = SpELValidator.class)
 @Target({ElementType.TYPE, ElementType.FIELD, ElementType.PARAMETER})
@@ -17,8 +42,18 @@ public @interface SpELAssert {
 
     String message() default "{io.github.ro4.spelvalidation.SpELAssert.message}";
 
+    /**
+     * @return The name, under which the annotated element shall be registered
+     * within the SpEL context. Defaults to "p".
+     */
     String alias() default "p";
 
+
+    /**
+     * @return The name of the property for which you would like to report a validation error.
+     * If given, the resulting constraint violation will be reported on the specified property.
+     * If not given, the constraint violation will be reported on the annotated bean.
+     */
     String reportOn() default "";
 
     Class<?>[] groups() default {};
