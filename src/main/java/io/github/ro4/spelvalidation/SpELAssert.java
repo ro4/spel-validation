68459f3080755cf3c107bb880eda13589466dbc5
ro4 <thefrp@foxmail.com>
2022-10-17 22:06:30 +0800

    ci cd

--- src/main/java/io/github/ro4/spelvalidation/SpELAssert.java
+++ src/main/java/io/github/ro4/spelvalidation/SpELAssert.java
@@ -7,23 +7,21 @@ import java.lang.annotation.*;
 /**
  * This is a custom validator based on `Jakarta validation-api`, inspired by `org.hibernate.validator.constraints.ScriptAssert`.
  * <pre>
- * {@code
- * @SpELAssert("#p.maxAge > 100") // repeatable annotation
- * @SpELAssert(expression = "#_.maxAge > 100", alias = "_") // alias support
- * @SpELAssert(
+ * {@code @SpELAssert("#p.maxAge > 100") // repeatable annotation}
+ * {@code @SpELAssert(expression = "#_.maxAge > 100", alias = "_") // alias support}
+ * {@code @SpELAssert(
  *         expression = {"#p.minAge < #p.maxAge", "and #p.minAge > 10"}, // multi-line expressions
  *         message = "Max must greater than min age, min age must greater than 10",
  *         reportOn = "minAge"
- * )
+ * )}
+ * {@code @SpELAssert("@springBean.method(#p.name)") // spring bean invoke}
  * public class Config {
  *
- *     @SpELAssert("@springBean.method(#p)") // spring bean invoke
  *     private String name;
  *
  *     private Integer minAge;
  *
  *     private Integer maxAge;
- * }
  * }
  * </pre>
  *
